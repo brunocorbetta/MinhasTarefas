339181a47e47399d4a39c91b951170d3970dddd8
bruno corbetta <corbetta510@gmail.com>
2024-02-25 20:46:43 -0300

    deletar tarefas ok

--- app/src/main/java/com/corbetta/minhastarefas/ui/Home.kt
+++ app/src/main/java/com/corbetta/minhastarefas/ui/Home.kt
@@ -2,23 +2,36 @@ package com.corbetta.minhastarefas.ui
 
 
 import android.annotation.SuppressLint
+import android.graphics.Paint.Align
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -30,7 +43,12 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -57,27 +75,52 @@ fun Home(
     Scaffold(
         floatingActionButton = {
             FloatingActionButton(
-                modifier = Modifier.padding(18.dp),
+                modifier = Modifier.padding(1.dp),
                 onClick = {
                           navController.navigate("AddTask")
                 },
             ) {
                 Icon(Icons.Filled.Add, contentDescription = "")
             }
-        }
+        },
     ) {
-        Column(Modifier.fillMaxSize()) {
-            Row(Modifier.fillMaxWidth()) {
-                Text(
-                    text = "Tarefas",
-                    fontSize = 26.sp,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(12.dp),
-                    textAlign = TextAlign.Center,
 
-                    )
-            }
+        Surface (
+            color = MaterialTheme.colorScheme.tertiary,
+            modifier = Modifier
+                .width(500.dp)
+                .height(80.dp)
+
+
+
+        ) {
+            Text(
+                text = "Tarefas",
+                style = MaterialTheme.typography.displaySmall,
+                color = MaterialTheme.colorScheme.onBackground,
+                overflow = TextOverflow.Clip,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .padding(top = 12.dp)
+                    .fillMaxWidth(),
+
+
+
+                )
+        }
+
+
+
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .padding(1.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+
+        ) {
+
+
+
 
             if (deleteConfirmationRequired) {
                 DeleteTask(
@@ -97,7 +140,10 @@ fun Home(
                     selectedItemId = itemId
                     deleteConfirmationRequired = true
 
-                }
+                },
+                modifier = Modifier
+                    .padding(1.dp)
+
 
             )
         }
@@ -108,8 +154,6 @@ fun Home(
 private fun HomeBody(
     taskList: List<Task>, onItemClick: (Int) -> Unit, modifier: Modifier = Modifier
         ) {
-
-
 
     Column(horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier) {
@@ -129,7 +173,7 @@ fun TaskList(
     onItemClick: (Int) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    LazyColumn(modifier = modifier) {
+    LazyRow(modifier = modifier) {
         items(items = taskList, key = { it.id }) {item ->
             TaskItem(task = item,
                 onItemClick = onItemClick)
@@ -142,21 +186,47 @@ fun TaskList(
 private fun TaskItem(
     task: Task, modifier: Modifier = Modifier,  onItemClick: (Int) -> Unit
 ) {
-    Card(
-        modifier = modifier.clickable { onItemClick(task.id) },
-        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+    ElevatedCard(
+
+        elevation = CardDefaults.cardElevation(defaultElevation = 12.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.tertiary,
+            contentColor = MaterialTheme.colorScheme.background
+        ),
+        shape = RoundedCornerShape(8.dp),
+
+
+        modifier = modifier
+            .width(250.dp)
+            .height(200.dp)
+            .padding(6.dp)
+            .clickable { onItemClick(task.id) }
+
 
     ) {
         Column(
 
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceBetween,
+            modifier = modifier.padding(6.dp)
+
         ) {
-            Row(
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Text(text = task.titulo)
-                Spacer(Modifier.weight(1f))
-                Text(text = task.descricao)
-            }
+                Text(
+                    text = task.titulo,
+                    style = MaterialTheme.typography.displaySmall,
+                    textAlign = TextAlign.Center,
+                    color = MaterialTheme.colorScheme.onSurface,
+                    modifier = modifier.padding(4.dp)
+
+                )
+                Text(
+                    text = task.descricao,
+                    style = MaterialTheme.typography.bodyLarge,
+                    textAlign = TextAlign.Justify,
+                    color = MaterialTheme.colorScheme.onSurface,
+                    modifier = modifier.padding(4.dp)
+
+                )
         }
     }
 }
